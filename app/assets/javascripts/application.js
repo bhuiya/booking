7341669fa12f3110e560aed20fe14002b2b3da98
bdb <bhuiya@github.com>
2018-07-16 18:02:31 -0400

    yarn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery/dist/jquery
 //= require toastr/build/toastr.min.js
 //= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
