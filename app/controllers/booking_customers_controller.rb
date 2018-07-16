5adc44f818596f8e9ca320f61ea03d0496750d21
bdb <bhuiya@github.com>
2018-07-16 17:55:36 -0400

    heroku

--- app/controllers/booking_customers_controller.rb
+++ app/controllers/booking_customers_controller.rb
@@ -6,19 +6,18 @@ class BookingCustomersController < ApplicationController
  end
 
  def create
-   @current_user =current_user.id.to_i
-   @registration= BookingCustomer.create(booking_id: params[:booking_id].to_i, customer_id: @current_user)
+  @current_user =current_user.id.to_i
+  @registration= BookingCustomer.create(booking_id: params[:booking_id].to_i, customer_id: @current_user)
    # byebug
-   if @registration.valid?
+  if @registration.valid?
     flash[:success] = 'Your guest event has been successfully created'
     redirect_to  userhome_index_path
-   else
+  else
     flash[:error] = 'Registion Error: cannot be registrar two event at the same time.'
-   end
   end
-
-  private
-   def booking_customers_params
-	  	params.require(:booking_customer).permit(:customer_id, :booking_id)
-	 end
+ end
+ private
+  def booking_customers_params
+  	params.require(:booking_customer).permit(:customer_id, :booking_id)
+  end
 end
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,8 +1,8 @@
 class CustomersController < ApplicationController
   def index
-       @bookings = Booking.all
+    @bookings = Booking.all
   end
   def new
-         @customer = Customer.new
+    @customer = Customer.new
   end
 end
