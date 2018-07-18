7ef00d3d464d68ef843ee4aa02136788b6fb6a55
bdb <bhuiya@github.com>
2018-07-17 20:51:20 -0400

    application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery/dist/jquery
+//= require jquery
 //= require toastr/build/toastr.min.js
 //= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
